0c9a45e9849d0eccbc3a14d75a4eab5f0948b3e6
adrtrn <adrian.s.tran@gmail.com>
2020-01-24 15:44:55 -0800

    adds a get request method for grabbing all listings from the db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const { retrieveCollection } = require('../db/index.js');
+
 const port = process.env.PORT || 3000;
 
 let app = express();
@@ -8,6 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname)));
+
+app.get('/api/v1/listings', (req, res) => {
+  retrieveCollection(result => {
+    res.send(result);
+  })
+})
 
 app.listen(port, () => console.log('listening on port: ' + port));
 
