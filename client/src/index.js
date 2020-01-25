e3688265cd700a37e4a5d1f3bf07fd4b0e4eea57
adrtrn <adrian.s.tran@gmail.com>
2020-01-24 18:13:06 -0800

    set up webpack and babel, css loaders and babel loaders

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
-function component() {
-  const element = document.createElement('div');
+// function component() {
+//   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'tuna tataki furakake miso karage';
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = 'tuna tataki furakake miso karage';
 
-  return element;
-}
+//   return element;
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(<h1>tuna tataki furakake miso karage</h1>, document.getElementById('app'));
