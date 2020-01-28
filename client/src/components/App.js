8267242f7a6a1717365fbf98478941912e4fdcf3
adrtrn <adrian.s.tran@gmail.com>
2020-01-28 15:39:52 -0800

    adds checkout component to be rendered

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Checkout from './Checkout.js';
 
  const App = () => {
   return (
-    <div>
-      <h1>tuna tataki furakake miso karage</h1>
+    <div className="App">
+      <Checkout />
     </div>
   );
 };
