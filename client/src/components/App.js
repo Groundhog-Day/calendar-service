8ab7e0a3d35c5606558a464a317843ccbc9a97e1
adrtrn <adrian.s.tran@gmail.com>
2020-01-30 00:34:49 -0800

    implements useEffect to make one fetch request to stop infinite renders

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Checkout from './Checkout';
+import Calendar from './Calendar';
 
 export default function App() {
   const [accommodation, setAccommodation] = useState(null);
+  const n = Math.floor(Math.random() * Math.floor(99))
 
-
-  fetch('/api/v1/listings').then((res) => {
-    return res.json();
-  }).then(accommodations => {
-    setAccommodation(accommodations[0])
-  })
+  useEffect(() => {
+    fetch('/api/v1/listings').then((res) => {
+      return res.json();
+    }).then(accommodations => {
+      setAccommodation(accommodations[n])
+    })
+  }, [])
 
   return (
     <div className="App">
+      {<Calendar />}
       {accommodation && <Checkout accommodation={accommodation} />}
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './checkout.css';
-import './calendar.css';
+import './styles/checkout.css';
+import './styles/calendar.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
