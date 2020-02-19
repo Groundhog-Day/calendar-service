bed25e17c35a9b1f3e9da13632de904ecbfadb19
Albert <asgk912@gmail.com>
2020-02-19 11:27:21 -0800

    now generate unique id value

--- db/psql/seedUserData.js
+++ db/psql/seedUserData.js
@@ -1,41 +1,47 @@
+// (sub)modules to connect with psql
 const { Client } = require('pg');
 const client = new Client();
 const copyFrom = require('pg-copy-streams').from;
 
+// (sub)modules to generate fake data and write into csv file
 const faker = require('faker');
 const fs = require('fs');
 const path = require('path');
 
-
-const gen100kUserData = (index, callback) => {
-  // create file and write header
-  const fileName = path.join(__dirname, `users${index}.csv`);
+/*  
+  START: Declare Helper Functions
+*/
+const generate100k = (ind1, ind2, callback) => {
+  // create a file and write header
+  const fileName = path.join(__dirname, `users${ind2}.csv`);
   fs.writeFileSync(fileName, 'id, username, name, email, about, location, work\n');
 
-  // create variables needed to generate fake data
-  const dataLimit = (10 ** 5);
-  const stringLengthLimit = (2 ** 25);
+  // create variables needed to generate fake data and write into csv file
+  const dataLimit = (10 ** 5);          // 100k
+  const stringLengthLimit = (2 ** 25);  // MDN says different browser has different length limit, and the minimum is (2 ** 27 - 1)
   let tempString = '';
 
 
-  for (let i = (index * dataLimit 0); i < (index+1) * dataLimit; i++) {
+  for (let i = 0; i < dataLimit; i++) {
     // generate fake data
-    let decider = Math.random();
+    let decider = Math.random(); // decide whether to have data for some attributes
 
-    let id = i;
+    let id = (ind1 * (dataLimit * 10)) + (ind2 * dataLimit) + i + 1; // M digit + 100k digit + rest of digits
     let username = faker.internet.userName();
     let name = faker.name.findName();
     let email = faker.internet.email();
     let about = (decider < 0.95) ? '""' : faker.lorem.sentence();
 
     let location = (decider < 0.8) ? '""' : faker.fake("{{address.streetAddress}}, {{address.city}}, {{address.country}}");
-    location = location.replace(/,/g, '');
+    location = location.replace(/,/g, ''); // cannot escape , somehow so just delete commas
 
     let work = (decider < 0.8) ? '""' : faker.fake("{{name.jobTitle}} at {{company.suffixes}}");
-    work = work.replace(/,/g, '');
+    work = work.replace(/,/g, ''); // cannot escape , somehow so just delete commas
+
 
     // add generated fake data to temp string
     tempString += `${id}, ${username}, ${name}, ${email}, ${about}, ${location}, ${work}\n`;
+
 
     // if either temp string is too long or the loop reached to the end, append to the file
     if(tempString.length > stringLengthLimit || i === dataLimit - 1) {
@@ -48,32 +54,35 @@ const gen100kUserData = (index, callback) => {
       });
     }
 
-    // copy to the database if the loop reached to the end, and delete the file
+    // copy to the database if the data generation loop reached to the end
     if(i === dataLimit - 1) {
+      // initiate pg-copy-stream
       let stream = client.query(copyFrom('COPY users FROM STDIN WITH CSV HEADER'));
       let fileStream = fs.createReadStream(fileName);
 
+      // event(?) listner 
       fileStream.on('error', (err) => console.log('Error in Reading: ', err));
       stream.on('error', (err) => console.log('Error in Copying: ', err));
-      stream.on('end', () => {
+      stream.on('end', () => { // upon copying data, delete csv file
         fs.unlink(fileName, (err) => {
           if (err) throw err;
           callback();
         });
       });
-      fileStream.pipe(stream);
 
-      
+      // pipe everything described above
+      fileStream.pipe(stream);
     }
   }
 }
 
-const gen1MUserData = (callback) => {
+
+const generate1M = (ind1, callback) => {
   let bound = 10;
   let count = 0;
 
-  for(let i = 0; i < bound; i++) {
-    gen100kUserData(i, () => {
+  for(let ind2 = 0; ind2 < bound; ind2++) {
+    generate100k(ind1, ind2, () => {
       count++;
       if (count === 10) {
         callback();
@@ -83,22 +92,26 @@ const gen1MUserData = (callback) => {
 }
 
 
-let countMillion = 0;
-let generateUpTo = 24;
+let countMillion = 0; // tracks how many data in million are generated
+let generateUpTo = 24; // decides the upper limit of how many data in million are generated
 
-const cbToReinvoke = () => {
+const reinvokeGen1M = () => {
   countMillion++;
   if (countMillion !== generateUpTo) {
-    gen1MUserData(cbToReinvoke);
+    generate1M(countMillion, reinvokeGen1M);
   } else {
     client.end();
   }
 }
+/*  
+  END: Declare Helper Functions
+*/
 
-client.connect();
 
-client.query('DROP TABLE IF EXISTS users CASCADE')
-  .then( () => (   // create all tables
+client.connect(); // connect to psql server
+
+client.query('DROP TABLE IF EXISTS users CASCADE') // drop table to delete all previously save data
+  .then( () => (   // create table again without constraints (constraints are neglected to use 'copy' method)
     client.query(
       `CREATE TABLE users (
         id INTEGER NOT NULL,
@@ -115,39 +128,5 @@ client.query('DROP TABLE IF EXISTS users CASCADE')
     client.end();
   })
   .then( () => {
-    gen1MUserData(cbToReinvoke);    
-  }
-
- /* PRIMARY DATA: Accomodation
-const writeTo = path.join(__dirname, 'accomodation.csv');
-fs.writeFileSync(writeTo, 'host, joinDate, address, city, bedroom, bed, baths, maxGuests, minDaysStay, checkInHour, checkOutHour, amenities, houseRules, cancelationPolicy, reviewCount, ratingScore, minCostPerNight, maxCostPerNight, serviceFee, cleaningee, occupancyTax\n');
-
-fs.appendFile(writeTo, `asdf, 123119, 44 Tehma Street, San Francisco, 2, 2, 1, 4, 1, 02:30 PM, 11:00 AM, 11111111, 01010101, blahblahblah, 234, 4.23, 120, 154, 40, 50, 30`, (err) => { 
-    if(err) {
-      console.log(err);
-    }
-});
-
-let host = ;
-let joinDate = ;
-const randNum = Math.random;
-let address = faker.address.streetAddress();
-let city = faker.address.county;
-let bedroom = randNum < 0.6 ? 1 : randNum < 0.8 ? 2 : randNum < 0.95 ? 3 : 4;
-let bed ;
-let baths = randNum < 0.95 ? 1 : 2;
-let maxGuests = bed + (randNum < 0.25 ? 1 : ;
-let minDaysStay = randNum < 0;
-let checkInHour;
-let checkOutHour;
-let amenities = Math.floor(randNum * 256).toString(2);
-let houseRules;
-let cancelationPolicy;
-let reviewCount;
-let ratingScore;
-let minCostPerNight;
-let maxCostPerNight;
-let serviceFee;
-let cleaningee;
-let occupancyTax;
-*/
+    generate1M(countMillion, reinvokeGen1M);    
+  });
