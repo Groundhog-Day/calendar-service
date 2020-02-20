41198a8fe193bb268bd686e6342c7c4bfd80e105
Albert <asgk912@gmail.com>
2020-02-19 16:16:46 -0800

    wrote schema

--- db/cassandra/seed.js
+++ db/cassandra/seed.js
@@ -1,12 +1,89 @@
 const cassandra = require('cassandra-driver');
  
 const client = new cassandra.Client({
-  contactPoints: ['h1', 'h2'],
+  contactPoints: ['127.0.0.1:9042'],
   localDataCenter: 'datacenter1',
-  keyspace: 'ks1'
+  keyspace: 'sdc'
 });
  
-const query = 'SELECT name, email FROM users WHERE key = ?';
- 
-client.execute(query, [ 'someone' ])
-  .then(result => console.log('User with email %s', result.rows[0].email));
+const dropTableQuery = 'DROP TABLE IF EXISTS calendar'
+const createTableQuery =
+  `CREATE TABLE calendar (
+    id int,
+    username TEXT,
+    name TEXT,
+    email TEXT,
+    about ASCII,
+    location ASCII,
+    work TEXT,
+    accomodationAddress ASCII PRIMARY KEY,
+    bedroom TINYINT,
+    bed TINYINT,
+    baths TINYINT,
+    maxGuests TINYINT,
+    minDayStaty TINYINT,
+    checkInHour TIME, 
+    checkOutHour TIME,
+    amenities TEXT,
+    houseRules TEXT,
+    cancelationPolicy ASCII,
+    reviewCount SMALLINT,
+    ratingScore FLOAT,
+    minCostPerNight TINYINT,
+    maxCostPerNight TINYINT,
+    serviceFee TINYINT,
+    cleaningFee TINYINT,
+    occupancyTax TINYINT,
+    clientUser TEXT,
+    startDate DATE,
+    endDate DATE,
+    adults TINYINT,
+    children TINYINT,
+    infants TINYINT,
+    paid BOOLEAN
+  )`;
+let id;
+let host_username;
+let host_name;
+let host_email;
+let host_about;
+let host_location;
+let host_work;
+let accomodationAddress;
+let bedroom;
+let bed;
+let baths;
+let maxGuests;
+let minDayStaty;
+let checkInHour;
+let checkOutHour;
+let amenities;
+let houseRules;
+let cancelationPolicy;
+let reviewCount;
+let ratingScore;
+let minCostPerNight;
+let maxCostPerNight;
+let serviceFee;
+let cleaningFee;
+let occupancyTax;
+let clientUser;
+let startDate;
+let endDate;
+let adults;
+let children;
+let infants;
+let paid;
+`${id},${host_username},${host_name},${host_email},${host_about},${host_location},${host_work},${accomodationAddress},${bedroom},${bed},${baths},${maxGuests},${minDayStaty},${checkInHour},${checkOutHour},${amenities},${houseRules},${cancelationPolicy},${reviewCount},${ratingScore},${minCostPerNight},${maxCostPerNight},${serviceFee},${cleaningFee},${occupancyTax},${clientUser},${startDate},${endDate},${adults},${children},${infants},${paid}`
+
+client.execute(dropTableQuery)
+  .then(() => ( client.execute(createTableQuery) ))
+  .then((result) => {
+    console.log(result);
+    client.shutdown();
+  })
+  .catch((err) => {
+    console.log(err);
+    client.shutdown();
+  })
+
