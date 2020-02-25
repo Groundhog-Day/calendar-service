948e13c34eaff537e53b3114e85b39e094713d98
Albert <asgk912@gmail.com>
2020-02-24 16:28:56 -0800

    final edition with k6 config file

--- script.js
+++ script.js
@@ -2,14 +2,33 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  vus: 1,
+  vus: 1000,
   duration: '300s'
 };
 
 export default function() {
-  let id = Math.ceil(Math.random() * (10 ** 7));
-  let url = `http://localhost:3000/api/v1/listings/${id}`;
+  let accomodation_id = Math.ceil(Math.random() * (10 ** 7));
+  let user_id = Math.ceil(Math.random() * 24 * (10 * 6));
 
-  let res = http.get(url);
+  let dateTracker = new Date();
+  dateTracker.setDate(dateTracker.getDate() + Math.floor(Math.random() * 8));
+  
+  let startDate = `${dateTracker.getFullYear()}-${(dateTracker.getMonth()+1).toString().padStart(2, '0')}-${dateTracker.getDate()}`;
+
+  dateTracker.setDate(dateTracker.getDate() + 1 + Math.floor(Math.random() * 3));
+  
+  let endDate = `${dateTracker.getFullYear()}-${(dateTracker.getMonth()+1).toString().padStart(2, '0')}-${dateTracker.getDate()}`;
+
+  let adults = 1 + Math.floor(Math.random() * 3);
+  let children = Math.floor(Math.random() * 4);
+  let infants = Math.floor(Math.random() * 3);
+
+  let paid = (Math.random() < 0.8);
+
+  
+  let url = `http://localhost:3000/api/v1/listings/${accomodation_id}/reservation`;
+  let payload = {user_id, startDate, endDate, adults, children, infants};
+
+  let res = http.post(url, payload);
   sleep(1);
 }
